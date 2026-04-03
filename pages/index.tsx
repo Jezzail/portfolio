9ee2bb5ee5f82688416345627a39810a7005da6d
Jezzail <pat43607@gmail.com>
2026-04-03 22:00:00 +0900

    Revise homepage for temporary downtime notice
    Updated homepage content to indicate temporary downtime and ongoing improvements.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,43 @@
 import type { NextPage } from "next";
-import { Grid, Text, Container, Row, Image } from "@nextui-org/react";
-
-import SocialItem from "../components/SocialItem";
-import { info } from "../info/info";
+import { Grid, Text, Container, Row } from "@nextui-org/react";
 
 import classes from "./index.module.css";
 
 const Home: NextPage = () => {
   return (
-    <Grid.Container gap={2} className={classes.gridContainer}>
-      <Grid className={classes.gridImage}>
-        <Image src="/me.png" className={classes.image} />
-      </Grid>
+    <Grid.Container gap={2} className={classes.gridContainer} justify="center">
       <Grid>
         <Text h1>
-          {"Hi! I'm "}
+          🚧{" "}
           <Text span className={classes.name}>
-            {info.about.name}
-          </Text>
-          👋
+            Portfolio
+          </Text>{" "}
+          is being rebuilt
         </Text>
-        <Text h2>{info.about.rol}</Text>
-        <Container gap={1} justify={"space-around"}>
-          {info.bio.map((text) => (
-            <Row key={text.id}>
-              <Text b className={classes.bio}>
-                {text.text}
-              </Text>
-            </Row>
-          ))}
+
+        <Text h2 css={{ textAlign: "center" }}>
+          Temporary downtime while I work on a new version
+        </Text>
+
+        <Container gap={1} justify={"center"}>
+          <Row justify="center">
+            <Text className={classes.bio}>
+              I'm currently redesigning and improving this site.
+            </Text>
+          </Row>
+          <Row justify="center">
+            <Text className={classes.bio}>
+              It will be back soon with updated projects and a better structure.
+            </Text>
+          </Row>
         </Container>
-        <Grid.Container gap={2} justify="center">
-          {info.socials.map((social) => (
-            <Grid key={social.label}>
-              <SocialItem social={social} />
-            </Grid>
-          ))}
+
+        <Grid.Container gap={2} justify="center" css={{ marginTop: "20px" }}>
+          <Grid>
+            <Text small css={{ opacity: 0.6 }}>
+              Thanks for your patience 🙏
+            </Text>
+          </Grid>
         </Grid.Container>
       </Grid>
     </Grid.Container>
